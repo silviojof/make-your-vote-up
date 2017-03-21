58182634bd59de4765c6e315591d6d17ff8fcbce
Wendi <wendi.chu04@gmail.com>
2017-03-20 17:00:59 -0700

    added img folder

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="css/styles.css" />
-  <title>Document Title</title>
+  <title>wendibranch</title>
 </head>
 <body>
   <div class="row colors">
-    <div class="medium-6 column">Left</div>
+    <div class="medium-6 column">fhfghfghgh</div>
     <div class="medium-6 column">Right</div>
   </div>
 </body>
