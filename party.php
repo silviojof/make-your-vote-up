cf8111cc783e6c93da4ef8de2dbbf5fcd8f4f3de
Wendi <wendi.chu04@gmail.com>
2017-03-22 19:35:59 -0700

    changes to party page

--- party.php
+++ party.php
@@ -10,10 +10,25 @@
 </head>
 <body>
   <div class="row">
-    <div class="small-12 medium-6 column">
+    <div class="party-page small-12 medium-12 large-6 column">
+      <div class="large-6 column hang-left">
         <img src="img/conservative_party.svg"/>
+        <div class="keep-bottom">
+          <img class="representative-party" src="img/steve-harper.jpg"/>
+        </div>
+    </div>
+    <div class="large-8 column representative">
+        <h2>REPRESENTATIVE</h2>
+        <p class="uppercase">STEVE HARPER<br/>
+            BORN: APRIL 30, 1959
+            RIDING: CALGARY HERITAGE
+            EDUCATION: ECONOMICS,
+            UNIVERSITY OF CALGARY</p>
+            <p>Rona Ambrose is the current interim leader of the Conservative Party. She was born in March 15,1969 in Valleyview, Alberta. Ambrose has a Masters of Arts degree in political science and a Bachelor of Arts in women’s and gender studies. Ambrose, a proclaimed feminist  has been involved with various organization working to end violence against women before her political career. </p>
+        <h2>MEMBERS OF PARLIAMENT</h2>
     </div>
 
+  </div>
 
   <script
     src="https://code.jquery.com/jquery-3.2.1.min.js"
