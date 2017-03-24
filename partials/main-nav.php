69fc5d85f76c74295c669fcde0e7c367d243fd74
Ailia Martin <ailia.vmd@gmail.com>
2017-03-23 17:16:20 -0700

    logo

--- partials/main-nav.php
+++ partials/main-nav.php
@@ -1,7 +1,9 @@
 <nav class="row main-nav expanded collapse">
     <div class="large-6 medium-12 columns">
         <div class="row site-nav">
-            <div class="medium-6 small-10 columns">logo</div>
+            <div class="medium-6 small-10 columns">
+                <a href="index.php"><img src="./img/logo/logo4.svg" alt="make-your-vote-up logo"></a>
+            </div>
             
             <div class="small-2 columns show-for-small-only" id="site-menu">menu</div>
             <div class="medium-3 columns hide-for-small-only site-menu">who's my mp</div>
