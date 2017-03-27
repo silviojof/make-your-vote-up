9ad76f20659e4133664d716f1f8e5ff8c2f563fc
Silvio Oliveira <silviojof@gmail.com>
2017-03-27 16:41:36 -0700

    index

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" type="text/css" href="css/styles.css" />
+        <script src="https://use.fontawesome.com/983b5cbe32.js"></script>
         <title>wendibranch</title>
     </head>
     <body>
@@ -24,7 +25,7 @@
                 <div class="large-3 medium-6 small-12 columns party" data-party=""></div>
                 <div class="large-3 medium-6 small-12 columns party" data-party=""></div>
             </div>
-        
+
         <?php include('partials/footer.php'); ?>
 
     </body>
